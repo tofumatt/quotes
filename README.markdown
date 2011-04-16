4553fd7094b527ff28cff1cfd9e0113f8b90bd45
Matthew Riley MacPherson <matt@lonelyvegan.com>
2011-04-16 18:01:37 -0300

    Initial commit; add the very basic README

---
+++ README.markdown
@@ -0,0 +1,12 @@
+= Quotes.txt =
+== Keeping track of the stupid things you and your friends say online that no one (else) should read ==
+
+Quotes.txt is a simple Django web app that allows you to post funny things you and your friends say in chat to a "quotes.txt" file (in reality, some kind of database) and review all of the funny things you've said over the years, but with zero context. It's inspired by the quotes.txt [@allanlawlor](http://twitter.com/allanlawlor) and I kept while I was working in government.
+
+This is different from quote database sites like [bash.org](http://bash.org) in a few important ways:
+
+	* Anyone can create a user account
+	* There are "friend groups" to allow only you and your friends to post quotes
+	* There is an HTTP API for posting your quotes in automated fashions
+
+The last part is handy if you run a chat in Campfire or on IRC and want to create a bot that will automatically post a recent quote to the site. I encourage the bot to respond to `>> quotes.txt`, because that's kind of hilarious.
---
+++ manage.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import os
+import site
+import sys
+
+
+ROOT = os.path.dirname(os.path.abspath(__file__))
+
+path = lambda *a: os.path.join(ROOT, *a)
+
+prev_sys_path = list(sys.path)
+
+site.addsitedir(path('apps'))
+site.addsitedir(path('lib'))
+site.addsitedir(path('vendor'))
+
+# Move the new items to the front of sys.path. (via virtualenv)
+new_sys_path = []
+for item in list(sys.path):
+    if item not in prev_sys_path:
+        new_sys_path.append(item)
+        sys.path.remove(item)
+sys.path[:0] = new_sys_path
+
+from django.core.management import execute_manager
+try:
+    import settings_local as settings
+except ImportError:
+    try:
+        import settings
+    except ImportError:
+        import sys
+        sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
+        raise
+
+if __name__ == "__main__":
+    execute_manager(settings)
