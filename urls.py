29de257b312d54626a6f2333f27042d2c0f5e6d5
Matthew Riley MacPherson <matt@lonelyvegan.com>
2011-04-16 19:10:31 -0300

    Include main urlconf and __init__ file

---
+++ urls.py
@@ -0,0 +1,27 @@
+from django.conf import settings
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    (r'', include('chats.urls')),
+    
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    
+    # Uncomment the next line to enable the admin:
+    (r'^admin/', include(admin.site.urls)),
+)
+
+if settings.DEBUG:
+    media_url = settings.MEDIA_URL.lstrip('/').rstrip('/')
+    urlpatterns += patterns('',
+        (r'^%s/(?P<path>.*)$' % media_url, 'django.views.static.serve',
+            {
+                'document_root': settings.MEDIA_ROOT,
+                'show_indexes': True
+            }
+        ),
+    )
