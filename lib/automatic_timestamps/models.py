982a36fed761f9948dfc85ab7e2334b80659198f
Matthew Riley MacPherson <matt@lonelyvegan.com>
2011-04-17 17:57:51 -0300

    Add automatic timestamp library for easy created/updated fields

---
+++ lib/automatic_timestamps/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+import datetime
+
+
+class TimestampModel(models.Model):
+    """
+    Extend the default Django model to add timestamps to all objects.
+    """
+    
+    class Meta:
+        abstract = True
+    
+    # Timestamps!
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    
+    def save(self):
+        """
+        Override the save method to automatically set the created_at and
+        updated_at fields with current date info.
+        """
+        if self.created_at == None:
+            self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+        
+        super(TimestampModel, self).save()
