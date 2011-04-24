5784aab6e63369ba6d60b7605811ee29f57d44e8
Matthew Riley MacPherson <matt@lonelyvegan.com>
2011-04-24 20:18:17 -0300

    Add chat templates/form; update layout to use title block

---
+++ apps/chats/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.models import User
+
+from chats.models import Chat
+from profiles.models import FriendGroup
+
+
+class PublicChatForm(forms.ModelForm):
+    """Public-facing Chat form used in the web-interface for users."""
+    
+    class Meta:
+        fields = ('text',)
+        model = Chat
