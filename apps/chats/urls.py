208955402fe3ecb77415454be5253c57c099dd6b
Matthew Riley MacPherson <matt@lonelyvegan.com>
2011-04-24 20:17:33 -0300

    Don't use / as the chat index

--- apps/chats/urls.py
+++ apps/chats/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    url('^$', 'chats.views.index', name='chats_index'),
+    url('^chats$', 'chats.views.index', name='chats_index'),
     url('^chats/new$', 'chats.views.new', name='chats_new'),
     url('^chats/(?P<id>\d+)$', 'chats.views.show', name='chats_show'),
 )
