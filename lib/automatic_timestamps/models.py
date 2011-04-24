065f4d744dc2035bf57eb35b0d3e123be0504908
Matthew Riley MacPherson <matt@lonelyvegan.com>
2011-04-24 20:30:33 -0300

    Actually _USE_ the args/kwargs in save()

--- lib/automatic_timestamps/models.py
+++ lib/automatic_timestamps/models.py
@@ -24,4 +24,4 @@ class TimestampModel(models.Model):
             self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
         
-        super(TimestampModel, self).save()
+        super(TimestampModel, self).save(*args, **kwargs)
