c37269b8e1ea62773a2af1a04676d6d74aac0850
Matthew Riley MacPherson <matt@lonelyvegan.com>
2011-04-25 22:08:56 -0300

    Use dashes in url names

--- apps/chats/urls.py
+++ apps/chats/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('chats.views',
-    url('^chats/$', 'index', name='chats_index'),
-    url('^chats/new/$', 'new', name='chats_new'),
-    url('^chats/(?P<id>\d+)/$', 'show', name='chats_show'),
+    url('^chats/$', 'index', name='chats-index'),
+    url('^chats/new/$', 'new', name='chats-new'),
+    url('^chats/(?P<id>\d+)/$', 'show', name='chats-show'),
 )
